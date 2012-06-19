17326be5408a1d784bb1fbc3c4e16d1f55ae9b68
bgardella <bgardella@yahoo.com>
2012-06-19 00:49:41 -0700

    dynamically add post-it buttons;

--- Documents/workspace/xcode/SyncedVideo/SyncedVideo/SyncedVideo/SyncedVideoViewController.m
+++ Documents/workspace/xcode/SyncedVideo/SyncedVideo/SyncedVideo/SyncedVideoViewController.m
@@ -129,6 +129,8 @@ static float PHONE_SCROLL_WIDTH     = 480;
         
         NSLog(@"adjusted width:%i", widthAdjusted);
         
+        [self addSongPackButtons:paths];
+        
     }else {
         [scrollView setScrollEnabled:NO];
         pageControl.hidden = YES;
@@ -137,6 +139,89 @@ static float PHONE_SCROLL_WIDTH     = 480;
 
 }
 
+- (void)addSongPackButtons:(NSArray *)songPackPaths{
+    
+    for (NSString *manifestFilePath in songPackPaths) {
+        
+        NSData *fileContents = [NSData dataWithContentsOfFile:manifestFilePath];
+        NSString *content = [NSString stringWithUTF8String:[fileContents bytes]];
+        NSArray *values = [content componentsSeparatedByCharactersInSet:[NSCharacterSet newlineCharacterSet]];
+        
+        NSString *buttonOne;
+        NSString *btnOneTitle;
+        NSString *buttonTwo;
+        NSString *btnTwoTitle;
+        NSString *packId;
+        
+        for(NSString *line in values){
+            if([line hasPrefix:@"button_1_id"]){
+                NSArray *arr = [line componentsSeparatedByString:@"="];
+                buttonOne = [arr objectAtIndex:1];
+            }
+            if([line hasPrefix:@"button_1_title"]){
+                NSArray *arr = [line componentsSeparatedByString:@"="];
+                btnOneTitle = [arr objectAtIndex:1];
+            }
+            if([line hasPrefix:@"button_2_id"]){
+                NSArray *arr = [line componentsSeparatedByString:@"="];
+                buttonTwo = [arr objectAtIndex:1];
+            }
+            if([line hasPrefix:@"button_2_title"]){
+                NSArray *arr = [line componentsSeparatedByString:@"="];
+                btnTwoTitle = [arr objectAtIndex:1];
+            }
+            if([line hasPrefix:@"pack_id"]){
+                NSArray *arr = [line componentsSeparatedByString:@"="];
+                packId = [arr objectAtIndex:1];
+            }
+            
+        }
+        
+        [self createSongPackButtons:buttonOne :btnOneTitle :buttonTwo :btnTwoTitle :packId];
+    }
+    
+}
+
+- (void)createSongPackButtons:  (NSString *)buttonId1 
+                             :  (NSString *)btnOneTitle 
+                             :  (NSString *)buttonId2
+                             :  (NSString *)btnTwoTitle
+                             :  (NSString *)packId{
+    //NSLog(@"CREATE button one: %@ button two: %@ pack id: %@", buttonId1, buttonId2, packId);
+    
+    if(packId.intValue > 0){
+
+        NSString *imgPath = [[NSBundle mainBundle] pathForResource:@"post_it_big" ofType:@"png"];
+        UIImage *image = [UIImage imageWithContentsOfFile:imgPath];
+        
+        
+        UIButton *songButton1 = [UIButton buttonWithType:UIButtonTypeCustom];
+        //[evtButton addTarget:self 
+        //              action:@selector(videoTouched) forControlEvents:UIControlEventTouchUpInside];
+        
+        [songButton1 setFrame:CGRectMake(1171,82,300,300)];
+        UILabel *label1 = [[UILabel alloc] initWithFrame:CGRectMake(31,2,300,300)];
+        label1.font = [UIFont fontWithName:@"Noteworthy" size:60];
+        label1.textColor = UIColor.blackColor;
+        label1.text = btnOneTitle;
+        label1.backgroundColor = [UIColor clearColor];
+        [songButton1 addSubview:label1];
+        [songButton1 setBackgroundImage:image forState:UIControlStateNormal];
+        [scrollView addSubview:songButton1];
+        
+        UIButton *songButton2 = [UIButton buttonWithType:UIButtonTypeCustom];
+        [songButton2 setFrame:CGRectMake(1571,82,300,300)];
+        UILabel *label2 = [[UILabel alloc] initWithFrame:CGRectMake(31,2,300,300)];
+        label2.font = [UIFont fontWithName:@"Noteworthy" size:60];
+        label2.textColor = UIColor.blackColor;
+        label2.text = btnTwoTitle;
+        label2.backgroundColor = [UIColor clearColor];
+        [songButton2 addSubview:label2];
+        [songButton2 setBackgroundImage:image forState:UIControlStateNormal];
+        [scrollView addSubview:songButton2];
+        
+    }
+}
 
 - (void)viewDidUnload {
     [super viewDidUnload];
